a62d07d7b6970aecd3f1562586812a2bbeacdcb7
thuhle <62361041+thuhle@users.noreply.github.com>
2020-05-03 00:10:35 -0400

    end html

--- PA06/hangman_webapp.py
+++ PA06/hangman_webapp.py
@@ -12,7 +12,9 @@ state = {'guesses':[],
          'word':'interesting',
 		 'word_so_far':'-----',
          'guesses_left':10,
-		 'done':False}
+		 'done':False,
+         'response':'',
+         'one_more_time':''}
 
 @app.route('/')
 @app.route('/main')
@@ -42,37 +44,42 @@ def hangman():
     	return play()
 
     elif request.method == 'POST':
-        # guesses_left=len(state['word'])
-        letter = request.form['guess']
-        if letter in state['guesses']:#check if letter has already been guessed
-            state['guesses_left']-=1
-            print("you've already guessed it")# and generate a response to guess again
-        elif letter not in state['word']:# else check if letter is in word
-            print("the letter is not in the word")
-            state['guesses'] += [letter]
-            state['guesses_left']-=1
-        else:
-            print("the letter is in the word")
-            state['guesses'] += [letter]
-
-        # state['guesses_left']
-
-        if len([x for x in state['word'] if x not in state['guesses']])==0:#"all the letters in the word have been guessed"
-            state['done']=True #"set done to be true and tell the user they won!"
-            print('you win!')
-
-        elif state['guesses_left']==0:
-            state['done']=True
-            print('you lost')#"set done to be true and tell the user they lost!"
-        else:
-            # print_word(word,guessed_letters)
-            word_so_far=hangman_app.get_word_so_far(state['word'],state['guesses'])
-            state['word_so_far']=word_so_far
+        while not state['done']:
+            # guesses_left=len(state['word'])
             letter = request.form['guess']
-        # then see if the word is complete
-        # if letter not in word, then tell them
-        # state['guesses']+=[letter]
-        return render_template('play.html',state=state)
+            if letter in state['guesses']:#check if letter has already been guessed
+                state['guesses_left']-=1
+                print("you've already guessed it")# and generate a response to guess again
+            elif letter not in state['word']:# else check if letter is in word
+                print("the letter is not in the word")
+                state['guesses'] += [letter]
+                state['guesses_left']-=1
+            else:
+                print("the letter is in the word")
+                state['guesses'] += [letter]
+
+            # state['guesses_left']
+
+            if len([x for x in state['word'] if x not in state['guesses']])==0:#"all the letters in the word have been guessed"
+                state['done']=True #"set done to be true and tell the user they won!"
+                state['response']="You have correctly guessed the word %s" %state['word']# print('you win!')
+
+            elif state['guesses_left']==0:
+                state['done']=True
+                state['response']="You are out of guesses. The word is %s" % state['word']# print('you lost')#"set done to be true and tell the user they lost!"
+            else:
+                # print_word(word,guessed_letters)
+                word_so_far=hangman_app.get_word_so_far(state['word'],state['guesses'])
+                state['word_so_far']=word_so_far
+                state['response']="Please guess again"
+                letter = request.form['guess']
+            # then see if the word is complete
+            # if letter not in word, then tell them
+            # state['guesses']+=[letter]
+            if state['done']==True:
+                return render_template('result.html',state=state)
+            else:
+                return render_template('play.html',state=state)
 
 
 
